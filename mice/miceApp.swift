6c97b571f10b2ce4c339294dcdcc57ecfa77e811
zeeker <zeekerpro@gmail.com>
2023-08-26 10:19:36 +0800

    Initial Commit

---
+++ mice/miceApp.swift
@@ -0,0 +1,17 @@
+//
+//  miceApp.swift
+//  mice
+//
+//  Created by zeeker # on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct miceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
