02e56cce1e74b3c3190f56d28dd45988f1309ab7
zeeker <zeekerpro@gmail.com>
2023-08-26 10:42:36 +0800

    v1.0.0

--- mice/ContentView.swift
+++ mice/ContentView.swift
@@ -1,26 +1,64 @@
 //
 //  ContentView.swift
-//  mice
+//  macgpt
 //
-//  Created by zeeker # on 8/26/23.
+//  Created by zeeker # on 8/24/23.
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @State private var urlString = "https://chat.openai.com/" // 替换为你想要打开的网页的URL
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(urlString: $urlString)
+        //Text("hello world")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WebView: View {
+    @Binding var urlString: String
+    var body: some View {
+        WebViewContainer(urlString: $urlString)
+    }
+}
+
+struct WebViewContainer: NSViewRepresentable {
+    @Binding var urlString: String
+
+    func makeNSView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
+    }
+
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            nsView.load(request)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebViewContainer
+
+        init(_ parent: WebViewContainer) {
+            self.parent = parent
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // 在这里可以处理加载完成后的操作
+        }
     }
 }
